912a4109fb179faffb75e1b84fce985ec080b367
spaek14 <>
2021-02-08 18:00:39 +0000

    max limit added

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,9 +1,20 @@
 class Oystercard
 
+LIM = 90.0
+
   attr_reader :balance
 
   def initialize
-    @balance = 0
+    @balance = 0.0
+  end
+
+  def top_up(money)
+    raise "balance cannot exceed #{LIM}" if (@balance + money) > LIM
+    @balance += money.to_f
+  end
+
+  def max_limit
+    LIM
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,17 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
+  it "top_up adds appropriate amount" do
+    expect(subject.top_up(5)).to eq(5)
+  end
+
+  it "has a maximum limit of 90" do
+    expect(subject.max_limit).to eq(90)
+  end
+
+  it "balance + money cannot be greater than limit" do
+    max_balance = Oystercard::LIM
+    subject.top_up(max_balance)
+    expect{ subject.top_up(1) }.to raise_error"balance cannot exceed #{max_balance}"
+  end
 end
