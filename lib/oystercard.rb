1eb09d9c4a4ff58b5471bc8f2cce2be061d62548
spaek14 <>
2021-02-09 15:40:27 +0000

    step 9

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,13 @@
 class Oystercard
 
 LIM = 90.0
+MINFARE = 1.0
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   def initialize
     @balance = 0.0
+    @in_journey = false
   end
 
   def top_up(money)
@@ -17,4 +19,20 @@ LIM = 90.0
     LIM
   end
 
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    raise "cannot touch in if balance is less than #{MINFARE}" if @balance < MINFARE
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,12 +2,18 @@ require 'oystercard'
 
 describe Oystercard do
 
+  before(:each) do
+    max_balance = Oystercard::LIM
+    subject.top_up(max_balance)
+  end
+
+
   it "has a balance of 0" do
-    expect(subject.balance).to eq(0)
+    expect(Oystercard.new.balance).to eq(0)
   end
 
   it "top_up adds appropriate amount" do
-    expect(subject.top_up(5)).to eq(5)
+    expect(Oystercard.new.top_up(5)).to eq(5)
   end
 
   it "has a maximum limit of 90" do
@@ -16,7 +22,32 @@ describe Oystercard do
 
   it "balance + money cannot be greater than limit" do
     max_balance = Oystercard::LIM
-    subject.top_up(max_balance)
     expect{ subject.top_up(1) }.to raise_error"balance cannot exceed #{max_balance}"
+  end
+
+  it "can deduct 5" do
+    limit = Oystercard::LIM
+    subject.deduct(5)
+    expect(subject.balance).to eq(limit - 5)
+  end
+
+  it "new instance of Oystercard.in_journey?" do
+    expect(subject.in_journey?).to eq(false)
+  end
+
+  it "can touch_in" do
+    subject.touch_in
+    expect(subject.in_journey?).to eq(true)
+  end
+
+  it "can touch_out" do
+    subject.touch_in
+    subject.touch_out
+    expect(subject.in_journey?).to eq(false)
+  end
+
+  it "can't touch_in if the balance is less than the minimum fare" do
+    min_fare = Oystercard::MINFARE
+    expect{ Oystercard.new.touch_in }.to raise_error"cannot touch in if balance is less than #{min_fare}"
   end
 end
